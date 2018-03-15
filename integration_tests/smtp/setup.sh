2ed16edeac5f816ab37f17b44cb56b00179d13db
anproagile <anproagile@gmail.com>
2018-03-15 13:17:52 -0400

    remove port forwarding from pop3 container

--- integration_tests/smtp/setup.sh
+++ integration_tests/smtp/setup.sh
@@ -1,23 +1,24 @@
 #!/usr/bin/env bash
 
-echo "pop3/setup: Tests setup for pop3"
+echo "smtp/setup: Tests setup for smtp"
 
-CONTAINER_TAG="zgrab_pop3"
-CONTAINER_NAME="zgrab_pop3"
+CONTAINER_TAG="zgrab_smtp"
+
+CONTAINER_NAME="zgrab_smtp"
 
 # If the container is already running, use it.
 if docker ps --filter "name=$CONTAINER_NAME" | grep -q $CONTAINER_NAME; then
-    echo "pop3/setup: Container $CONTAINER_NAME already running -- nothing to setup"
+    echo "smtp/setup: Container $CONTAINER_NAME already running -- nothing to setup"
     exit 0
 fi
 
 # If it is not running, try launching it -- on success, use that. 
-echo "pop3/setup: Trying to launch $CONTAINER_NAME..."
+echo "smtp/setup: Trying to launch $CONTAINER_NAME..."
 if ! docker run --rm --name $CONTAINER_NAME -td $CONTAINER_TAG; then
-    echo "pop3/setup: Building docker image $CONTAINER_TAG..."
+    echo "smtp/setup: Building docker image $CONTAINER_TAG..."
     # If it fails, build it from ./container/Dockerfile
     docker build -t $CONTAINER_TAG ./container
     # Try again
-    echo "pop3/setup: Launching $CONTAINER_NAME..."
+    echo "smtp/setup: Launching $CONTAINER_NAME..."
     docker run --rm --name $CONTAINER_NAME -td $CONTAINER_TAG
 fi
