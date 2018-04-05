63934126b3d468fe17f20f34cc7e026ab574a2f9
anproagile <anproagile@gmail.com>
2018-04-05 14:19:58 -0400

    add --retry-https flag to work with unknown HTTP/HTTPS servers; remove unused request.tls field from zschema (only used for HTTP servers, caused zschema errors because of ListOf(ListOf()))

--- processing.go
+++ processing.go
@@ -11,7 +11,6 @@ import (
 	"time"
 
 	log "github.com/sirupsen/logrus"
-	"github.com/zmap/zgrab2/lib/output"
 )
 
 // Grab contains all scan responses for a single host
@@ -104,16 +103,8 @@ func grabTarget(input ScanTarget, m *Monitor) []byte {
 		ipstr = s
 	}
 
-	raw := Grab{IP: ipstr, Domain: input.Domain, Data: moduleResult}
-
-	// TODO FIXME: Move verbosity to global level, or add a Verbosity() method to the Module interface.
-	stripped, err := output.Process(raw)
-	if err != nil {
-		log.Warnf("Error processing results: %v", err)
-		stripped = raw
-	}
-
-	result, err := json.Marshal(stripped)
+	a := Grab{IP: ipstr, Domain: input.Domain, Data: moduleResult}
+	result, err := json.Marshal(a)
 	if err != nil {
 		log.Fatalf("unable to marshal data: %s", err)
 	}
