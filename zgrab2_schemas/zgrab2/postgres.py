b7302101736f9ee269b8f6bbf7848aa6961a5f5a
anproagile <anproagile@gmail.com>
2018-08-22 11:49:17 -0400

    Improve partial results for mongodb scanner

--- zgrab2_schemas/zgrab2/postgres.py
+++ zgrab2_schemas/zgrab2/postgres.py
@@ -14,9 +14,9 @@ import zgrab2
 # These are defined in detail at
 #   https://www.postgresql.org/docs/10/static/protocol-error-fields.html
 postgres_error = SubRecord({
-    "severity": WhitespaceAnalyzedString(),
+    "severity": WhitespaceAnalyzedString(required=True),
     "severity_v": WhitespaceAnalyzedString(),
-    "code": WhitespaceAnalyzedString(),
+    "code": WhitespaceAnalyzedString(required=True),
     "message": WhitespaceAnalyzedString(),
     "detail": WhitespaceAnalyzedString(),
     "hint": WhitespaceAnalyzedString(),
@@ -27,7 +27,6 @@ postgres_error = SubRecord({
     "schema": WhitespaceAnalyzedString(),
     "table": WhitespaceAnalyzedString(),
     "data": WhitespaceAnalyzedString(),
-    "constraint": WhitespaceAnalyzedString(),
     "file": WhitespaceAnalyzedString(),
     "line": WhitespaceAnalyzedString(),
     "routine": WhitespaceAnalyzedString(),
@@ -59,7 +58,7 @@ postgres_scan_response = SubRecord({
         "supported_versions": WhitespaceAnalyzedString(),
         "protocol_error": postgres_error,
         "startup_error": postgres_error,
-        "is_ssl": Boolean(),
+        "is_ssl": Boolean(required=True),
         "authentication_mode": postgres_auth_mode,
         # TODO FIXME: This is currendly an unconstrained map[string]string
         "server_parameters": WhitespaceAnalyzedString(),
